15b425197648aa3d59a93e067423d5357e978d91
Vladyslav Smahin <vladislavsmagin1@gmail.com>
2024-06-16 17:00:39 +0300

    Питання 1
    Завдання 3: Перевірка парності чисел

--- 004/main.js
+++ 004/main.js
@@ -14,3 +14,11 @@ for (let i = 0; i < array.length; i++) {
 }
 
 console.log("Сумма элементов массива: " + sum);
+
+for ( let i = 1 ; i <= 10 ; i++ ) {
+    if ( i % 2 === 0 ) {
+        console.log(i + " парне");
+    } else {
+        console.log(i + "непарне");
+    }
+}
