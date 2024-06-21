5084c8a52dba107a38af477366eb39a467e8c6ad
Vladyslav Smahin <vladislavsmagin1@gmail.com>
2024-06-21 14:27:46 +0300

    006.1

---
+++ 006/main.js
@@ -0,0 +1,16 @@
+true + false;         // 1
+8 / "2";              // 4
+"number" + 5 + 1;     // "number51"
+5 + 1 + "number";     // "6number"
+7 && 2;               // 2
+2 && 7;               // 7
+null + 1;             // 1
+undefined + 1;        // NaN
+"five" + + "two";     // 'fiveNaN'
+'true' == true;       // false
+false == 'false';     // false
+null == '';           // false
+!!"false" == !!"true";// true
+"4" - 3;              // 1
+"4px" - 3;            // NaN
+0 || "0" && 1;        // 1
