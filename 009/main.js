a08d7ab9e515c2bcefeb7a078aff03819277ff59
Vladyslav Smahin <vladislavsmagin1@gmail.com>
2024-06-26 17:12:48 +0300

    009.2

--- 009/main.js
+++ 009/main.js
@@ -1,10 +1,26 @@
-function calculateArea (width, height) {
+function calculateArea(width, height) {
     return width * height
 }
+
 console.log(
-    calculateArea(10,2),
-    calculateArea(1,2),
-    calculateArea(10,20)
+    calculateArea(10, 2),
+    calculateArea(1, 2),
+    calculateArea(10, 20)
 );
+
 ////
+
+function printMessage(message) {
+    console.log(message)
+}
+
+function sendMessage(message, callback) {
+    callback(message)
+}
+
+sendMessage('Hi, guys', printMessage);
+sendMessage('yo', printMessage);
+sendMessage('dattebayo', printMessage);
+sendMessage('    1', printMessage);
+//////
 
