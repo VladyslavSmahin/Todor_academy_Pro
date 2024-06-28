e6401282728fb44b26777221f1f445af961a9dc1
Vladyslav Smahin <vladislavsmagin1@gmail.com>
2024-06-28 19:44:56 +0300

    010

--- 010/main.js
+++ 010/main.js
@@ -7,7 +7,7 @@ let callbackConcept = `Основная концепция колбек функ
 function doSomethingElse () {
     console.log('3/синхронный doSomethingElse');
     setTimeout(()=>{
-        console.log('5/Асинхронный doSomethingElse')
+        console.log('5/Асинхронный doSomethingElse');
     }, 3000)
     console.log('7/синхронный doSomethingElse');
 }
@@ -18,10 +18,35 @@ function doSomethingElse2 (callback) {
         setTimeout(()=>{
             console.log('14/Асинхронный setTimeout 2 doSomethingElse2');
         }, 3000);
-        console.log('16/Асинхронный setTimeout doSomethingElse2')
+        console.log('16/Асинхронный setTimeout doSomethingElse2');
     }, 3000);
     console.log('18/синхронный doSomethingElse2');
     callback()
 }
 doSomethingElse2(doSomethingElse)
 console.log(`Колбек это : ${callbackFunc}, ${callbackConcept}, пример колбека: ${doSomethingElse} как аргумент в функции ${doSomethingElse2}`)
+
+/////////////////
+
+function sum (a, b, sumProcessing= (res) => res){
+    const res = a + b;
+    return sumProcessing(res)
+}
+function display (value){
+    console.log(value)
+}
+sum(2,5,display);
+
+/////////////////
+
+const p = new Promise(function (resolve, reject){
+    let name = "ержан";
+    setTimeout(function (){
+        (typeof name === "string" && name.length > 0) ? resolve(name.charAt(0).toUpperCase() + name.slice(1)) : reject(name)
+    },3000)
+});
+p.then(function (value){
+    console.log(`Имя ${value} отформатировано`)
+}).catch((function (){
+    console.error(`Введен некорректный тип данных`)
+}))
