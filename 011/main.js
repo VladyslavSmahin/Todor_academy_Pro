c3dbf1b144aec6d41974e6bf4e1f6ffd2874630e
Vladyslav Smahin <vladislavsmagin1@gmail.com>
2024-07-02 18:32:10 +0300

    011.2

--- 011/main.js
+++ 011/main.js
@@ -1,4 +1,4 @@
-let a = 0;
+/*let a = 0;
 
 function example1() {
     let a = 1;
@@ -8,10 +8,10 @@ function example1() {
     function example2() {
         let a = 2;
         console.log(`переопределенная в example2() а - ${a}`);
-        example3()
+        example3();
 
         function example3() {
-            example4()
+            example4();
             let a = 3;
             console.log(`переопределенная в example3() а - ${a}`);
             example4();
@@ -25,4 +25,28 @@ function example1() {
 }
 
 example1();
-console.log(`example2(), example3(), example4() не видны в глобальной области видимости, поэтому их вызов приведет к ошибке`)
+console.log(`example2(), example3(), example4() не видны
+в глобальной области видимости, поэтому их вызов приведет к ошибке`);*/
+///////////
+squareOfSum(1,2,square);
+squareOfSum1(1,2,square1);
+function squareOfSum (a,b, Sum = (res) => res ) {
+    const res = a+b;
+    square(10);
+    return Sum(res);
+}
+    function square (value) {
+  console.log(value*value);
+    }
+
+function squareOfSum1 (a,b, Sum = (res) => res ) {
+    const res = a+b;
+    square1(10);
+    return Sum(res);
+}
+const square1  = (value) => {
+    console.log(value*value);
+}
+console.log(`Функция squareOfSum работает в случае ее вызова до обьявления благодаря всплытию
+в отличии от функции squareOfSum1, где square1 обьявлена как function expression`);
+///////
