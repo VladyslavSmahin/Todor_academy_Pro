f03c72cf7730329a369460a998315164964b88ef
Vladyslav Smahin <vladislavsmagin1@gmail.com>
2024-07-02 20:48:01 +0300

    slider module

--- 011/main.js
+++ 011/main.js
@@ -28,7 +28,7 @@ example1();
 console.log(`example2(), example3(), example4() не видны
 в глобальной области видимости, поэтому их вызов приведет к ошибке`);*/
 ///////////
-squareOfSum(1,2,square);
+/*squareOfSum(1,2,square);
 squareOfSum1(1,2,square1);
 function squareOfSum (a,b, Sum = (res) => res ) {
     const res = a+b;
@@ -48,5 +48,44 @@ const square1  = (value) => {
     console.log(value*value);
 }
 console.log(`Функция squareOfSum работает в случае ее вызова до обьявления благодаря всплытию
-в отличии от функции squareOfSum1, где square1 обьявлена как function expression`);
+в отличии от функции squareOfSum1, где square1 обьявлена как function expression`);*/
 ///////
+
+var string1 = 'global variable';
+let string2 = 'local variable';
+const massive = [1, 2, 3];
+console.log(`
+     ${string1},
+     ${string2},
+     ${massive}
+`);
+
+string1 = '1';                       ///может быть переопределена
+string2 = '2';                       ///может быть переопределена
+///massive = [2, 3, 4];              ///приведет к ошибке,т.к. напрямую нельзя изменить const;
+console.log(`
+     ${string1},
+     ${string2},
+     ${massive}
+`);
+
+function display() {
+    string1 = '10';
+    string2 = '20';
+ ///massive = [2, 3, 4];                       ///приведет к ошибке,т.к. напрямую нельзя изменить const;
+    const massive = [20, 30, 40];     /// не приведет к ошибке,т.к. это совершенно другая константа
+    // (находится исключительно в области видимости функции display)
+    console.log(`
+     ${string1},       ///   переменная переопределилась 
+     ${string2},       ///   переменная переопределилась 
+     ${massive}        ///   была создана новая переменная 
+`);
+}
+
+display();
+
+console.log(`
+     ${string1},       /// переменная изменила свое значение после переопределеня в функции display
+     ${string2},       /// переменная изменила свое значение после переопределеня в функции display
+     ${massive}        /// осталась неизменной 
+`);
