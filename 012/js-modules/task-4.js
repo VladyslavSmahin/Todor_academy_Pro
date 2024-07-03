66091503464600bd54bad43bdb24c4d79932942a
Vladyslav Smahin <vladislavsmagin1@gmail.com>
2024-07-03 16:16:59 +0300

    012.4

---
+++ 012/js-modules/task-4.js
@@ -0,0 +1,12 @@
+export function task_4() {
+    function generateUniqueId() {
+    let id = ''
+        function getRandomId() {
+        console.log(id = Math.random())
+        }
+        getRandomId()
+        getRandomId()
+        console.log(id)
+    }
+    generateUniqueId()
+}
