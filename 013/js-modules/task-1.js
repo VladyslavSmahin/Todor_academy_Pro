ccaac08dde606103fe174bf1db38039b992e729d
Vladyslav Smahin <vladislavsmagin1@gmail.com>
2024-07-04 13:51:30 +0300

    013.1

---
+++ 013/js-modules/task-1.js
@@ -0,0 +1,6 @@
+export function task_1() {
+    function multiplyByTwo(arg) {
+        return arg * 2;
+    }
+    console.log(multiplyByTwo(4))
+}
---
+++ 013/js-modules/task-2.js
@@ -0,0 +1,4 @@
+export function task_2 (){
+
+
+}
