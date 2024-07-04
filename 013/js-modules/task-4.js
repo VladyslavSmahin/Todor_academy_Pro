92dd7cb95e36ad6e519190880b53afc74e5b376f
Vladyslav Smahin <vladislavsmagin1@gmail.com>
2024-07-04 14:35:03 +0300

    013

--- 013/js-modules/task-4.js
+++ 013/js-modules/task-4.js
@@ -1,4 +1,8 @@
-export function task_4 (){
-
-
+export function task_4() {
+    const  calculateAverage = (massive) =>
+        (massive.reduce((accumulator, currentValue) => accumulator + currentValue, 0)) / massive.length;
+    const massive1 = [1, 2, 3, 4];
+    const massive2 = [2, 3, 4];
+    console.log(calculateAverage(massive1));
+    console.log(calculateAverage(massive2));
 }
