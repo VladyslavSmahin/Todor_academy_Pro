f151ba3c98727a26433fe6d531e6bf0b99b83b28
Vladyslav Smahin <vladislavsmagin1@gmail.com>
2024-07-04 14:19:47 +0300

    013.3

--- 013/js-modules/task-3.js
+++ 013/js-modules/task-3.js
@@ -1,4 +1,15 @@
-export function task_3 (){
-
-
+export function task_3() {
+    function updateUserInfo(person, newName) {
+        return {
+            ...person,
+            name: newName,
+        };
+    }
+    const user1 = {name: 'Joseph', age: 100};
+    const user2 = updateUserInfo(user1, 'Donald')
+    function getUserInfo(user) {
+        return user.name
+    }
+    console.log(getUserInfo(user1));
+    console.log(getUserInfo(user2));
 }
