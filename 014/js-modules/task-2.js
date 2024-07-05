c2f9e0c99edd911663d3b61ceef589b606c199f8
Vladyslav Smahin <vladislavsmagin1@gmail.com>
2024-07-05 16:49:57 +0300

    014.2/014.3

--- 014/js-modules/task-2.js
+++ 014/js-modules/task-2.js
@@ -1,3 +1,9 @@
 export function task_2() {
+        function applyFunction(operation, arg) {
+            return operation(arg);
+        }
 
+        const massive = [1, 2, 3, 4];
+        const reducedMassive = applyFunction(massive => massive.reduce((acc, val) => acc+val, 0), massive);
+        console.log(reducedMassive)
 }
