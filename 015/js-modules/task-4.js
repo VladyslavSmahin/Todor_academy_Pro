36ac80d68bab2cb327d81287be97361a0d6fae57
Vladyslav Smahin <vladislavsmagin1@gmail.com>
2024-07-08 17:44:55 +0300

    015.4

--- 015/js-modules/task-4.js
+++ 015/js-modules/task-4.js
@@ -1,3 +1,19 @@
 export function task_4() {
-
+    let car = {
+        _brand: null,
+        _model: null,
+        description() {
+            return `${this._brand} ${this._model}`
+        },
+        set brand(value) {
+            this._brand  = value;
+        },
+        set model(value) {
+            this._model  = value;
+        }
+    }
+    console.log(car.description());
+    car.brand = 'audi';
+    car.model = 'tt';
+    console.log(car.description());
 }
