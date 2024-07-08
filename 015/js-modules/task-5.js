485001edbf8facdb4b7b01885c9d93f84b3d012f
Vladyslav Smahin <vladislavsmagin1@gmail.com>
2024-07-08 18:03:46 +0300

    015.5

--- 015/js-modules/task-5.js
+++ 015/js-modules/task-5.js
@@ -1,3 +1,18 @@
 export function task_5() {
-
+    let product = {
+        _name: '',
+        _price: '',
+        set name(value) {
+            this._name  = value;
+        },
+        set price(value) {
+            this._price  = value;
+        },
+        applyDiscount(discount) {
+            return this._price -  this._price*discount/100;
+        },
+    }
+    product.name = 'Кавунчик';
+    product.price = '200'
+    console.log(product.applyDiscount(50));
 }
