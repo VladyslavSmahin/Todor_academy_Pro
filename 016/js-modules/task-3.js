8aeb2d40874d9ad2c3b1bff4ca76b5787964c228
Vladyslav Smahin <vladislavsmagin1@gmail.com>
2024-07-12 13:45:28 +0300

    016.3

--- 016/js-modules/task-3.js
+++ 016/js-modules/task-3.js
@@ -1,3 +1,17 @@
 export function task_3() {
-
+    let car = {
+        brand: 'ВАЗ',
+        model: '2104',
+        year: 1999,
+    }
+    Object.preventExtensions('car');
+    console.log(Object.isExtensible('car'));
+    //Object.defineProperty('car','color',{});
+     ///TypeError: Object.defineProperty called on non-object at Function.defineProperty (<anonymous>)
+    let deepCopy = JSON.parse(JSON.stringify(car));
+    console.log(deepCopy);
+    Object.seal(deepCopy);
+    console.log(Object.isSealed('deepCopy'));
+    delete deepCopy.year;
+    console.log(deepCopy); /// TypeError: Cannot delete property 'year'
 }
