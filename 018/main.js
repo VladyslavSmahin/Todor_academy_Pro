3fe56662770a1cb9a489d4f22ca91f6e35a85069
Vladyslav Smahin <vladislavsmagin1@gmail.com>
2024-08-06 17:13:14 +0300

    018

--- 018/main.js
+++ 018/main.js
@@ -1,8 +1,11 @@
 import {task_1_2} from "./js-modules/task-1-2.js";
 import {task_3} from "./js-modules/task-3.js";
+import {task_4} from "./js-modules/task-4.js";
+import {task_5} from "./js-modules/task-5.js";
+
 document.addEventListener('DOMContentLoaded', () => {
-
-
     task_1_2();
     task_3();
+    task_4();
+    task_5();
 });
