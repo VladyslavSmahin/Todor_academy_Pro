2f50f2852ba9b378cf86db435930130ab6d27acb
Vladyslav Smahin <vladislavsmagin1@gmail.com>
2024-09-06 16:41:18 +0300

    027.1

--- 027/main.js
+++ 027/main.js
@@ -1,1 +1,31 @@
-//вс код форматирование тегов
+function fetchData(url) {
+    return new Promise((resolve, reject) => {
+        fetch(url)
+            .then(response => {
+                if (!response.ok) {
+                    throw new Error(`HTTP error! Status ${response.status}`);
+                }
+                return response.json();
+            })
+            .then(data => resolve(data))
+            .catch(error => reject(error.message))
+    })
+}
+
+const apiUrl = "https://swapi.dev/api/people/1/";
+
+fetchData(apiUrl)
+    .then(data => {
+        const p = document.createElement('p');
+        document.body.appendChild(p);
+        let result = '';
+        for (const key in data) {
+            if (data.hasOwnProperty(key)) {
+                result += `${key}: ${data[key]}`;
+            }
+        }
+        p.textContent = result
+    })
+    .catch(err => {
+        console.error(`Error: ${err}`)
+    })
