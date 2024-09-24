af9387da49d6212923a48bdd9adff71ff02d3f68
Vladyslav Smahin <vladislavsmagin1@gmail.com>
2024-09-24 22:01:23 +0300

    035.2

--- 31-50/035/main.js
+++ 31-50/035/main.js
@@ -1,4 +1,4 @@
-let xhr = new XMLHttpRequest();
+/*let xhr = new XMLHttpRequest();
 
 xhr.open('GET', 'https://todor.academy/', true);
 
@@ -35,7 +35,7 @@ xhrPost.send(data);
 
 let xhrPut = new XMLHttpRequest();
 
-xhrPut.open('PUT','https://todor.academy/', true);
+xhrPut.open('PUT', 'https://todor.academy/', true);
 
 xhrPut.setRequestHeader('Content-Type', '\'application/json');
 
@@ -56,14 +56,50 @@ xhrPut.send(updateData);
 
 let xhrDelete = new XMLHttpRequest();
 
-xhrDelete.open('DELETE','https://todor.academy/user/1', true);
+xhrDelete.open('DELETE', 'https://todor.academy/user/1', true);
 
 xhr.onload = function () {
     if (xhr.status === 200) {
-        console.log('Ресурс удален:', xhr.responseText);
+        console.log('Ресурс удален');
     } else {
         console.error('Ошибка', xhr.status);
     }
 };
 
-xhrDelete.send();
+xhrDelete.send();*/
+
+
+function apiRequest (url, method, data = null) {
+    const options = {
+        method,
+        headers: {
+            'Content-type': 'application/json'
+        }
+    }
+
+    if ( data) {
+        options.body = JSON.stringify(data);
+    }
+
+    return fetch(url, options)
+        .then(response => {
+            if (response.ok) {
+                // Определяем формат ответа по заголовку Content-Type
+                const contentType = response.headers.get('Content-Type');
+                if (contentType && contentType.includes('application/json')) {
+                    return response.json();
+                } else {
+                    return response.text(); // Для HTML или других типов данных
+                }
+            } else {
+                console.log(`Ошибка: ${response.status}`);
+            }
+        })
+        .then(data => console.log(data))
+        .catch(err => console.error(err));
+}
+
+apiRequest('https://todor.academy/', 'GET');
+apiRequest('https://todor.academy/user/1', 'POST', { name: 'Новый ресурс' });
+apiRequest('https://todor.academy/user/1', 'PUT', { name: 'Обновленный ресурс' });
+apiRequest('https://todor.academy/user/1', 'DELETE');
